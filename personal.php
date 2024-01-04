94b3a134d39ca54b9f2a44d178e0e2233eb8f08e
JesusAnd <jesusandradesustaita@gmail.com>
2024-01-04 10:42:43 -0600

    personal.php

---
+++ personal.php
@@ -0,0 +1,164 @@
+<?php include("headerprincipal.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo isset($tituloPagina) ? $tituloPagina : "Auditoría"; ?></title>
+    <link rel="stylesheet" type="text/css" href="librerias/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="librerias/fontawesome/css/all.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <style>
+      /* Estilos del encabezado */
+   
+      
+
+      .logo a {
+        color: #5E6160; /* Color del texto del logo */
+      }
+
+      .nav-link {
+        color: #5E6160; /* Color del texto del enlace de navegación */
+      }
+
+      .nav-link:hover {
+        color: #007bff; /* Color del texto del enlace de navegación al pasar el ratón */
+      }
+
+      /* Estilos de la sección del héroe */
+      #hero {
+        background-color: #545957; /* Color de fondo de la sección del héroe */
+        color: #fff; /* Color del texto de la sección del héroe */
+        padding: 60px 0; /* Espaciado interno de la sección del héroe */
+        text-align: center; /* Alineación del texto en el centro */
+      }
+
+      #hero h1 {
+        font-size: 3em; /* Tamaño de fuente del título principal */
+        margin-bottom: 20px; /* Espaciado inferior del título principal */
+      }
+
+      #hero h2 {
+        font-size: 1.5em; /* Tamaño de fuente del subtítulo */
+        margin-bottom: 40px; /* Espaciado inferior del subtítulo */
+      }
+
+      .icon-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #5E6160;
+    padding: 20px;
+    border-radius: 10px;
+    transition: 0.3s;
+    border-width: 3px;
+    border-style: solid;
+    border-color: #347357;
+    color: #ffffff;
+}
+
+.icon-box:hover {
+    background-color: #515353;
+    color: #ffffff;
+}
+
+.icon-box i {
+    font-size: 2em;
+    margin-bottom: 5px; /* Reduzco el espaciado inferior de los íconos */
+}
+
+.icon-box h3 {
+    font-size: 1.2em;
+    margin-bottom: 0; /* Elimino el espaciado inferior del título de la caja de iconos */
+}
+
+.icon-box a {
+    color: #ffffff;
+}
+
+.icon-box:hover a {
+    color: #000000;
+}
+
+.nav-link:hover {
+    color: #007bff;
+}
+
+    </style>
+</head>
+<body> 
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <!-- ... (tu contenido actual del encabezado) ... -->
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center justify-content-center">
+    <div class="container" data-aos="fade-up">
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-xl-6 col-lg-8">
+         
+          <h2>DEPARTAMENTO DE PERSONAL</h2>
+        </div>
+      </div>
+      <div class="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-store-line"></i>
+            <h3><a href="#">FUERZA DE TRABAJO​</a></h3>
+            
+            
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-store-line"></i>
+            <h3><a href="#">DOTACIÓN DE RH​</a></h3>
+            
+          </div>
+        </div>
+        
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-store-line"></i>
+            <h3><a href="#">PRESTACIONES AL PERSONAL</a></h3>
+            
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-store-line"></i>
+            <h3><a href="#">RETIRO LABORAL</a></h3>
+            
+          </div>
+        </div>
+        
+        <br>    
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ 
+ 
+
+      </div>
+    </div>
+    
+  </section><!-- End Hero -->
+
+ 
+</body>
+</html>
