a3ff1bb0eee06b131a2933f54e95b34ae732e5da
HernandezEmiliano <guez.emilio92@gmail.com>
2024-01-05 11:39:13 -0600

    Departamentos

--- gasto.php
+++ gasto.php
@@ -1,107 +1,122 @@
-<?php include("headerprincipal.php"); ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title><?php echo isset($tituloPagina) ? $tituloPagina : "Auditoría"; ?></title>
-    <link rel="stylesheet" type="text/css" href="librerias/bootstrap4/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="librerias/fontawesome/css/all.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo isset($tituloPagina) ? $tituloPagina : "Auditoría"; ?></title>
+  <link rel="stylesheet" type="text/css" href="librerias/bootstrap4/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="librerias/fontawesome/css/all.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <style>
-      /* Estilos del encabezado */
-   
-      .logo a {
-        color: #5E6160; /* Color del texto del logo */
-      }
+  <!-- Vendor CSS Files -->
+  <style>
+    /* Estilos del encabezado */
+    .logo a {
+      color: #5E6160;
+      /* Color del texto del logo */
+    }
 
-      .nav-link {
-        color: #5E6160; /* Color del texto del enlace de navegación */
-      }
+    .nav-link {
+      color: #5E6160;
+      /* Color del texto del enlace de navegación */
+    }
 
-      .nav-link:hover {
-        color: #007bff; /* Color del texto del enlace de navegación al pasar el ratón */
-      }
+    .nav-link:hover {
+      color: #007bff;
+      /* Color del texto del enlace de navegación al pasar el ratón */
+    }
 
-      /* Estilos de la sección del héroe */
-      #hero {
-        background-color: #545957; /* Color de fondo de la sección del héroe */
-        color: #fff; /* Color del texto de la sección del héroe */
-        padding: 60px 0; /* Espaciado interno de la sección del héroe */
-        text-align: center; /* Alineación del texto en el centro */
-      }
+    /* Estilos de la sección del héroe */
+    #hero {
+      background-color: #545957;
+      /* Color de fondo de la sección del héroe */
+      color: #fff;
+      /* Color del texto de la sección del héroe */
+      padding: 60px 0;
+      /* Espaciado interno de la sección del héroe */
+      text-align: center;
+      /* Alineación del texto en el centro */
+    }
 
-      #hero h1 {
-        font-size: 3em; /* Tamaño de fuente del título principal */
-        margin-bottom: 20px; /* Espaciado inferior del título principal */
-      }
+    #hero h1 {
+      font-size: 3em;
+      /* Tamaño de fuente del título principal */
+      margin-bottom: 20px;
+      /* Espaciado inferior del título principal */
+    }
 
-      #hero h2 {
-        font-size: 1.5em; /* Tamaño de fuente del subtítulo */
-        margin-bottom: 40px; /* Espaciado inferior del subtítulo */
-      }
+    #hero h2 {
+      font-size: 1.5em;
+      /* Tamaño de fuente del subtítulo */
+      margin-bottom: 40px;
+      /* Espaciado inferior del subtítulo */
+    }
 
-      .icon-container {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-      }
+    .icon-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
 
-      .icon-box {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: #5E6160;
-        padding: 20px;
-        border-radius: 10px;
-        transition: 0.3s;
-        border-width: 3px;
-        border-style: solid;
-        border-color: #347357;
-        color: #ffffff;
-        clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-        margin: 10px;
-      }
+    .icon-box {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background-color: #5E6160;
+      padding: 20px;
+      border-radius: 10px;
+      transition: 0.3s;
+      border-width: 3px;
+      border-style: solid;
+      border-color: #347357;
+      color: #ffffff;
+      clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+      margin: 10px;
+    }
 
-      .icon-box:hover {
-        background-color: #515353;
-        color: #ffffff;
-      }
+    .icon-box:hover {
+      background-color: #515353;
+      color: #ffffff;
+    }
 
-      .icon-box i {
-        font-size: 2em;
-        margin-bottom: 5px; /* Reduzco el espaciado inferior de los íconos */
-      }
+    .icon-box i {
+      font-size: 2em;
+      margin-bottom: 5px;
+      /* Reduzco el espaciado inferior de los íconos */
+    }
 
-      .icon-box h3 {
-        font-size: 1.2em;
-        margin-bottom: 0; /* Elimino el espaciado inferior del título de la caja de iconos */
-      }
+    .icon-box h3 {
+      font-size: 1.2em;
+      margin-bottom: 0;
+      /* Elimino el espaciado inferior del título de la caja de iconos */
+    }
 
-      .icon-box a {
-        color: #ffffff;
-      }
+    .icon-box a {
+      color: #ffffff;
+    }
 
-      .icon-box:hover a {
-        color: #000000;
-      }
+    .icon-box.selected {
+      background-color: #007bff;
+      color: #ffffff;
+    }
 
-      .nav-link:hover {
-        color: #007bff;
-      }
-      .icon-container .second-row .icon-box {
-  background-color: #e74c3c; /* Cambia el color de fondo para la segunda fila */
-  height: 150px; /* Ajusta la altura según tus necesidades */
-}
+    .nav-link:hover {
+      color: #007bff;
+    }
 
-    </style>
+    .icon-container .second-row .icon-box {
+      background-color: #e74c3c;
+      /* Cambia el color de fondo para la segunda fila */
+      height: 150px;
+      /* Ajusta la altura según tus necesidades */
+    }
+  </style>
 </head>
-<body> 
+
+<body>
 
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
@@ -119,53 +134,82 @@
       </div>
       <br>
       <br>
-      <div class="icon-container" data-aos="zoom-in" data-aos-delay="250">
-        <!-- Primera fila de iconos -->
-        <div class="icon-box">
-          <i class="ri-store-line"></i>
-          <h3><a href="#">   8.1   </a></h3>
-        </div>
-        <div class="icon-box">
-          <i class="ri-store-line"></i>
-          <h3><a href="#">   8.2   </a></h3>
-        </div>
-        <div class="icon-box">
-          <i class="ri-store-line"></i>
-          <h3><a href="#">   8.3   </a></h3>
-        </div>
-        
-        <!-- Segunda fila de iconos -->
-        <div class="icon-box second-row">
-          <i class="ri-store-line"></i>
-          <h3><a href="#">   8.4   </a></h3>
-        </div>
-        <div class="icon-box second-row">
-          <i class="ri-store-line"></i>
-          <h3><a href="#">   8.5   ​</a></h3>
-        </div>
-   
-      </div>
+      <center>
+
+        <table>
+          <tr>
+            <th class="icon-box" onclick="toggleIcon(this)">
+              <i class="ri-store-line"></i>
+              <h3><a href="#"> 8.1 </a></h3>
+            </th>
+            <th></th>
+            <th class="icon-box" onclick="toggleIcon(this)">
+              <i class="ri-store-line"></i>
+              <h3><a href="#"> 8.2 </a></h3>
+            </th>
+            <th></th>
+            <th class="icon-box" onclick="toggleIcon(this)">
+              <i class="ri-store-line"></i>
+              <h3><a href="#"> 8.3 </a></h3>
+            </th>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="icon-box" onclick="toggleIcon(this)">
+              <i class="ri-store-line"></i>
+              <h3><a href="#"> 8.4 </a></h3>
+            </td>
+            <td></td>
+            <td class="icon-box" onclick="toggleIcon(this)">
+              <i class="ri-store-line"></i>
+              <h3><a href="#"> 8.5 </a></h3>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </center>
+
     </div>
     <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- 
- 
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
   </section><!-- End Hero -->
 
+  <script>
+    function toggleIcon(element) {
+      element.classList.toggle('selected');
+    }
+  </script>
+
 </body>
+
 </html>
